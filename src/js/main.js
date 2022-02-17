f2af5851890c79ea49cd042e9f6a2e0c57027a5c
Szymon Okulowski <szymon.okulowski@gmail.com>
2022-02-17 20:10:42 +0100

    add about section

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector('.nav__btn-item');
 const nav = document.querySelector('.nav__mobile');
 const navList = document.querySelectorAll('.nav__mobile .nav__list a');
+const body = document.querySelector('body');
 
 const showNav = () => {
 	nav.classList.toggle('show-nav');
@@ -8,8 +9,10 @@ const showNav = () => {
 
 	if (nav.classList.contains('show-nav')) {
 		btnImg.setAttribute('src', './dist/img/icon-close.svg');
+		body.style.overflowY = 'hidden';
 	} else {
 		btnImg.setAttribute('src', './dist/img/icon-hamburger.svg');
+		body.style.overflowY = 'visible';
 	}
 };
 
